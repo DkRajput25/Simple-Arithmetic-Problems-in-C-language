d45f9e1e8e0433970cb1cd7496ab1ef35319aa67
DkRajput25 <94213659+DkRajput25@users.noreply.github.com>
2022-03-06 11:25:46 +0530

    Add files via upload

---
+++ Prime_number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void main()
+{
+    int i, j, k;
+    int num = 0;
+    printf("Enter a number: ");
+    scanf("%d", &k);
+    for (i = 2; i <= k/3; i++)
+    {
+        if (k % i == 0)
+        {
+            num = 1;
+            break;
+        }
+    }
+    if (num == 1 || k==1 ||k==0)
+    {
+        printf("Number is Not Prime Number\n");
+    }
+    else
+    {
+        printf("Number is Prime Number\n");
+    }
+}
